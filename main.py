552c489989087612a5383bdad3b5120256393291
sh-o <shog.ysd@gmail.com>
2019-10-20 20:14:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import time
 def main():
 
     frequency = 200
-    cycle_time = 600
+    repeat_count = 600
     gpio_pin = 18
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio_pin, GPIO.OUT)
 
-    for i in [20, 99, 20, 99]: #range(20, 100):
-        on_time = (1 / frequency) * (i / 100)
-        off_time = (1 / frequency) * (1 - (i / 100))
+    for i in [20, 99, 20, 99]:
+        
+        if 0 == i:
+            on_time = 0
+            off_time = (1 / frequency)
+        else:
+            on_time = (1 / frequency) * (i / 100)
+            off_time = (1 / frequency) * (1 - (i / 100))
 
-        for ct in range(cycle_time):
+        for ct in range(repeat_count):
             GPIO.output(gpio_pin, True)
             time.sleep(on_time)
             GPIO.output(gpio_pin, False)
